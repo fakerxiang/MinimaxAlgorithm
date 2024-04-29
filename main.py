92e4b78ef7423802c9924ab4c7e23043b453bc12
fakerxiang <fakerxiang@gmail.com>
2024-04-29 18:25:42 +0800

    2024-04-29 18:25:42

--- main.py
+++ main.py
@@ -142,3 +142,4 @@ def sample_sequence(*, hparams, length, start_token=None, batch_size=None, conte
 
         _, _, tokens = tf.while_loop(
             cond=cond, body=body,
+            maximum_iterations=length - 1,
