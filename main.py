227a7eca9b1f1d8350c32a1cc09c97965dfadcf2
fakerxiang <fakerxiang@gmail.com>
2024-06-19 00:50:40 +0800

    2024-06-19 00:50:40

--- main.py
+++ main.py
@@ -189,3 +189,4 @@ def top_p_logits(logits, p):
         tf.range(0, batch),
         # number of indices to include
         tf.maximum(tf.reduce_sum(tf.cast(cumulative_probs <= p, tf.int32), axis=-1) - 1, 0),
+    ], axis=-1)
