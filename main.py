db5f68e53e9a463636407398f563d83c6cec41ef
fakerxiang <fakerxiang@gmail.com>
2024-07-03 00:44:55 +0800

    2024-07-03 00:44:55

--- main.py
+++ main.py
@@ -206,3 +206,4 @@ def sample_sequence(*, hparams, length, start_token=None, batch_size=None, conte
         context = tf.fill([batch_size, 1], start_token)
 
     def step(hparams, tokens, past=None):
+        lm_output = model.model(hparams=hparams, X=tokens, past=past, reuse=tf.AUTO_REUSE)
