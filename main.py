a5b7a6578250c2398f10638305954e60b36e671d
fakerxiang <fakerxiang@gmail.com>
2024-09-29 01:23:16 +0800

    2024-09-29 01:23:16

--- main.py
+++ main.py
@@ -276,3 +276,4 @@ def sample_sequence(*, hparams, length, start_token=None, batch_size=None, conte
     def step(hparams, tokens, past=None):
         lm_output = model.model(hparams=hparams, X=tokens, past=past, reuse=tf.AUTO_REUSE)
 
+        logits = lm_output['logits'][:, :, :hparams.n_vocab]
