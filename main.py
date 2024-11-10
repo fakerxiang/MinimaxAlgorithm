dc6ff4652644f7797277ac8f023a06835aa46dd8
fakerxiang <fakerxiang@gmail.com>
2024-11-10 17:59:25 +0800

    2024-11-10 17:59:25

--- main.py
+++ main.py
@@ -307,3 +307,4 @@ def sample_sequence(*, hparams, length, start_token=None, batch_size=None, conte
             maximum_iterations=length - 1,
             loop_vars=[
                 past,
+                prev,
